35e46d25f04f56f9594404bb95d29464988e2b3a
samchuang <chichenchuang@gmail.com>
2017-03-14 16:29:15 +0800

    dbconfig

--- tinyMVC-Qstyle/tinymvc/myapp/configs/config_database.php
+++ tinyMVC-Qstyle/tinymvc/myapp/configs/config_database.php
@@ -12,9 +12,9 @@
 $config['default']['plugin'] = 'TinyMVC_PDO'; // plugin for db access
 $config['default']['type'] = 'mysql';      // connection type
 $config['default']['host'] = 'localhost';  // db hostname
-$config['default']['name'] = 'db name';     // db name
-$config['default']['user'] = 'db username';     // db username
-$config['default']['pass'] = 'db password';     // db password
+$config['default']['name'] = 'dbname';     // db name
+$config['default']['user'] = 'dbusername';     // db username
+$config['default']['pass'] = 'dbpassword';     // db password
 $config['default']['persistent'] = false;  // db connection persistence?
 
 ?>
